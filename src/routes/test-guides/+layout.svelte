6a14be88ef1822846fcb356b29eb672324a8d0bf
Stephen Stha <stephencrestha07@gmail.com>
2025-01-08 22:22:02 +0545

    third

--- src/routes/test-guides/+layout.svelte
+++ src/routes/test-guides/+layout.svelte
@@ -1,19 +1,23 @@
 <script>
-    import "./style.css";
-    import TGBreadcrumb from "../../components/TGBreadCrumb.svelte";
-    import TGRight from "../../components/TGRight.svelte";
+  import "./style.css";
+  import TGBreadcrumb from "../../components/TGBreadCrumb.svelte";
+  import TGRight from "../../components/TGRight.svelte";
+
+  import { page } from "$app/stores";
+  // Destructure pathname from $page.url
+  let { pathname } = $page.url;
 </script>
 
 <div class="test-guides-layout">
-    <div class="test-guides-breadcrumb">
-        <TGBreadcrumb />
+  <div class="test-guides-breadcrumb">
+    <TGBreadcrumb />
+  </div>
+  <div class="test-guides-row">
+    <div class="test-guides-left">
+      <slot></slot>
     </div>
-    <div class="test-guides-row">
-        <div class="test-guides-left">    
-            <slot></slot>
-        </div>
-        <div class="test-guides-right">
-            <TGRight />
-        </div>
+    <div class="test-guides-right">
+      <TGRight />
     </div>
+  </div>
 </div>
