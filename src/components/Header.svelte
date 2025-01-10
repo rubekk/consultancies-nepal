cd46b22c7ba4d7a870c8171d695fff689b109d81
Stephen Stha <stephencrestha07@gmail.com>
2025-01-10 17:00:39 +0545

    update

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,63 +1,59 @@
 <script>
-    import {
-        Navbar,
-        NavBrand,
-        NavLi,
-        NavUl,
-        NavHamburger,
-        Dropdown,
-        DropdownItem
-    } from "flowbite-svelte";
-    import { ChevronDownOutline } from "flowbite-svelte-icons";
+  import {
+    Navbar,
+    NavBrand,
+    NavLi,
+    NavUl,
+    NavHamburger,
+    Dropdown,
+    DropdownItem,
+  } from "flowbite-svelte";
+  import { ChevronDownOutline } from "flowbite-svelte-icons";
 </script>
 
 <Navbar class="border-b border-solid border-gray">
-    <NavBrand href="/">
-        <img
-            src="/favicon.png"
-            class="me-3 h-6 sm:h-9"
-            alt="StudyAbroadNp Logo"
-        />
-        <span
-            class="self-center whitespace-nowrap text-xl font-semibold text-black dark:text-white"
-            >Study<span class="text-[#2a73f4]"> Abroad</span>Np</span
-        >
-    </NavBrand>
-    <NavHamburger />
-    <NavUl>
-        <NavLi class="cursor-pointer">
-            Study Destinations
-            <ChevronDownOutline
-                class="w-6 h-6 ms-2 text-primary-800 dark:text-white inline"
-            />
-        </NavLi>
-        <Dropdown class="w-44 z-20">
-            <DropdownItem href="/study-abroad/australia">Study in Australia</DropdownItem>
-            <DropdownItem href="/study-abroad/usa">Study in USA</DropdownItem>
-            <DropdownItem href="/study-abroad/canada">Study in Canada</DropdownItem>
-            <DropdownItem href="/study-abroad/united-kingdom">Study in UK</DropdownItem>
-            <DropdownItem href="/study-abroad/new-zealand">Study in New Zealand</DropdownItem>
-            <DropdownItem href="/study-abroad/japan">Study in Japan</DropdownItem>
-            <DropdownItem href="/study-abroad/south-korea">Study in South Korea</DropdownItem>
-            <DropdownItem href="/study-abroad/singapore">Study in Singapore</DropdownItem>
-            <DropdownItem href="/study-abroad/germany">Study in Germany</DropdownItem>
-            <DropdownItem href="/study-abroad/europe">Study in Europe</DropdownItem>
-        </Dropdown>
-        <NavLi href="/universities">Universities</NavLi>
-        <NavLi class="cursor-pointer">
-            Test Guides
-            <ChevronDownOutline
-                class="w-6 h-6 ms-2 text-primary-800 dark:text-white inline"
-            />
-        </NavLi>
-        <Dropdown class="w-44 z-20">
-            <DropdownItem href="/test-guides/ielts">IELTS</DropdownItem>
-            <DropdownItem href="/test-guides/pte">PTE</DropdownItem>
-            <DropdownItem href="/test-guides/sat">SAT</DropdownItem>
-            <DropdownItem href="/test-guides/toefl">TOEFL</DropdownItem>
-            <DropdownItem href="/test-guides/gre">GRE</DropdownItem>
-            <DropdownItem href="/test-guides/gmat">GMAT</DropdownItem>
-        </Dropdown>
-        <NavLi href="/consultancies">Consultancies</NavLi>
-    </NavUl>
+  <NavBrand href="/">
+    <img src="/favicon.png" class="me-3 h-6 sm:h-9" alt="StudyAbroadNp Logo" />
+    <span
+      class="self-center whitespace-nowrap text-xl font-semibold text-black dark:text-white"
+      >Study<span class="text-[#2a73f4]"> Abroad</span>Np</span
+    >
+  </NavBrand>
+  <NavHamburger />
+  <NavUl>
+    <NavLi class="cursor-pointer">
+      Study Destinations
+      <ChevronDownOutline
+        class="w-6 h-6 ms-2 text-primary-800 dark:text-white inline"
+      />
+    </NavLi>
+    <Dropdown class="w-44 z-20">
+      <DropdownItem href="/study-abroad/australia">Australia</DropdownItem>
+      <DropdownItem href="/study-abroad/usa">USA</DropdownItem>
+      <DropdownItem href="/study-abroad/canada">Canada</DropdownItem>
+      <DropdownItem href="/study-abroad/united-kingdom">UK</DropdownItem>
+      <DropdownItem href="/study-abroad/new-zealand">New Zealand</DropdownItem>
+      <DropdownItem href="/study-abroad/japan">Japan</DropdownItem>
+      <DropdownItem href="/study-abroad/south-korea">South Korea</DropdownItem>
+      <DropdownItem href="/study-abroad/singapore">Singapore</DropdownItem>
+      <DropdownItem href="/study-abroad/germany">Germany</DropdownItem>
+      <DropdownItem href="/study-abroad/europe">Europe</DropdownItem>
+    </Dropdown>
+    <NavLi href="/universities">Universities</NavLi>
+    <NavLi class="cursor-pointer">
+      Test Guides
+      <ChevronDownOutline
+        class="w-6 h-6 ms-2 text-primary-800 dark:text-white inline"
+      />
+    </NavLi>
+    <Dropdown class="w-44 z-20">
+      <DropdownItem href="/test-guides/ielts">IELTS</DropdownItem>
+      <DropdownItem href="/test-guides/pte">PTE</DropdownItem>
+      <DropdownItem href="/test-guides/sat">SAT</DropdownItem>
+      <DropdownItem href="/test-guides/toefl">TOEFL</DropdownItem>
+      <DropdownItem href="/test-guides/gre">GRE</DropdownItem>
+      <DropdownItem href="/test-guides/gmat">GMAT</DropdownItem>
+    </Dropdown>
+    <NavLi href="/consultancies">Consultancies</NavLi>
+  </NavUl>
 </Navbar>
--- src/routes/consultancies/+page.svelte
+++ src/routes/consultancies/+page.svelte
@@ -1,6 +1,6 @@
 <script>
-    import Card from "../../components/Card.svelte";
-    import { data } from "$lib/index.js";
+  import Card from "../../components/Card.svelte";
+  import { data } from "$lib/index.js";
 </script>
 
 <svelte:head>
@@ -8,38 +8,42 @@
 </svelte:head>
 
 <div class="consultancies">
-    <div class="page-title">List of Consultancies</div>
-    <div class="consultancies-inner">
-        {#each data as indData}
-        <Card name={indData.name} address={indData.address? indData.address: "Putalisadak, Kathmandu"} imgSrc={indData.thumbnailImage? indData.thumbnailImage : ""}/>
-        {/each}
-    </div>
+  <div class="page-title">List of Consultancies</div>
+  <div class="consultancies-inner">
+    {#each data as indData}
+      <Card
+        name={indData.name}
+        address={indData.address ? indData.address : "Putalisadak, Kathmandu"}
+        imgSrc={indData.thumbnailImage ? indData.thumbnailImage : ""}
+      />
+    {/each}
+  </div>
 </div>
 
 <style>
-    .consultancies{
-        padding: 3rem 2rem 4rem;
-    }
-    .page-title{
-        margin: 0 auto 2.5rem;
-        width: max-content;
-        text-align: center;
-        font-size: 2rem;
-        border-bottom: 2px solid var(--blue);
-    }
-    .consultancies-inner{
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        flex-wrap: wrap;
-        gap: 15px;
-    }
-    h4{
-        margin: 3rem 0 0;
-        text-align: center;
-    }
-    h4 a{
-        color: blue;
-        text-decoration: underline;
-    }
+  .consultancies {
+    padding: 3rem 2rem 4rem;
+  }
+  .page-title {
+    margin: 0 auto 2.5rem;
+    width: max-content;
+    text-align: center;
+    font-size: 2rem;
+    border-bottom: 2px solid var(--blue);
+  }
+  .consultancies-inner {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-wrap: wrap;
+    gap: 15px;
+  }
+  h4 {
+    margin: 3rem 0 0;
+    text-align: center;
+  }
+  h4 a {
+    color: blue;
+    text-decoration: underline;
+  }
 </style>
