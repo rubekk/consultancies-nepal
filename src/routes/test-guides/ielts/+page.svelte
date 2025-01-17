7e9ed709ad2cddb0535e290e35058ddbc83e14ab
Stephen Stha <stephencrestha07@gmail.com>
2025-01-18 01:02:41 +0545

    updated

--- src/routes/test-guides/ielts/+page.svelte
+++ src/routes/test-guides/ielts/+page.svelte
@@ -1,11 +1,25 @@
+<script>
+  import {
+    List,
+    Li,
+    Heading,
+    Table,
+    TableBody,
+    TableBodyCell,
+    TableBodyRow,
+    TableHead,
+    TableHeadCell,
+  } from "flowbite-svelte";
+</script>
+
 <svelte:head>
   <title>StudyAbroadNP - Everything You Need to Know About IELTS</title>
 </svelte:head>
 
 <div class="test-container">
-  <h1>
-    Everything You Need to Know About IELTS: Test Pattern, Relevance, and Costs
-  </h1>
+  <Heading tag="h4"
+    >Everything You Need to Know About IELTS: Test Pattern, Relevance, and Costs</Heading
+  >
   <img src="/imgs/ielts.jpeg" alt="IELTS Image" />
   <p>
     The IELTS (International English Language Testing System) is a popular
@@ -17,68 +31,108 @@
     work, or move to English-speaking countries.
   </p>
 
-  <h2>Understanding the IELTS Test Pattern</h2>
+  <br />
+  <Heading tag="h5">Understanding the IELTS Test Pattern</Heading>
   <p>
     The IELTS test evaluates your English skills in four key areas: Listening,
     Reading, Writing, and Speaking. Here’s a breakdown of what each section
     entails:
   </p>
-  <p>
-    <span class="bold">1. Listening (30 minutes):</span> Answer 40 questions based
-    on four recordings with varied accents and contexts.
-  </p>
-  <p>
-    <span class="bold">2. Reading (60 minutes):</span> Complete 40 questions across
-    three texts from books, magazines, or newspapers, testing different reading skills.
-  </p>
-  <p>
-    <span class="bold">3. Writing (60 minutes):</span> The writing section is divided
-    into two tasks. Task 1: Describe visual data (graphs/charts). Task 2: Write an
-    essay on a given topic.
-  </p>
-  <p>
-    <span class="bold">4. Speaking (11-14 minutes):</span> A face-to-face interview
-    with three parts: introduction, topic discussion, and two-way dialogue.
-  </p>
+  <br />
+  <Table>
+    <TableHead>
+      <TableHeadCell>Pattern</TableHeadCell>
+      <TableHeadCell>Details</TableHeadCell>
+    </TableHead>
+    <TableBody tableBodyClass="divide-y">
+      <TableBodyRow>
+        <TableBodyCell>Listening (30 minutes)</TableBodyCell>
+        <TableBodyCell
+          >Answer 40 questions based on four recordings with varied accents and
+          contexts.</TableBodyCell
+        >
+      </TableBodyRow>
+      <TableBodyRow>
+        <TableBodyCell>Reading (60 minutes)</TableBodyCell>
+        <TableBodyCell
+          >Complete 40 questions across three texts from books,or magazines,<br
+          /> testing different reading skills.</TableBodyCell
+        >
+      </TableBodyRow>
+      <TableBodyRow>
+        <TableBodyCell>Writing (60 minutes)</TableBodyCell>
+        <TableBodyCell
+          >Divided into two tasks: <br />Task 1: Describe visual data
+          (graphs/charts). <br /> Task 2: Write an essay on a given topic.</TableBodyCell
+        >
+      </TableBodyRow>
+      <TableBodyRow>
+        <TableBodyCell>Speaking (11-14 minutes)</TableBodyCell>
+        <TableBodyCell
+          >A face-to-face interview with three parts: <br />introduction,
+          <br />topic discussion, <br />and two-way dialogue.</TableBodyCell
+        >
+      </TableBodyRow>
+    </TableBody>
+  </Table>
 
-  <h2>Cost of Taking the IELTS Exam</h2>
+  <br />
+  <Heading tag="h5">Cost of Taking the IELTS Exam</Heading>
   <p>
     The cost of the IELTS exam varies by country and test center, typically
     ranging from $215 to $240. Here’s a more detailed breakdown:
   </p>
-  <p>
-    <span class="bold">1. Standard Test Fee:</span> Covers the test and result issuance.
-  </p>
-  <p>
-    <span class="bold">2. Additional Costs:</span> Additional copies of results may
-    cost around $20 each.
-  </p>
-  <p>
-    <span class="bold">3. Cancellation and Rescheduling:</span> Partial refunds may
-    be available for cancellations over five weeks before the test; rescheduling
-    fees also apply.
-  </p>
-  <p>
-    <span class="bold">4. Preparation Costs:</span> Budget for preparation materials
-    or courses to boost your score.
-  </p>
+  <br />
 
-  <h2>Top Consultancies for IELTS</h2>
-  <ul class="text-black-500 pl-5">
-    <li><a href="/consultancies/education-planet">Education Planet</a></li>
-    <li>
-      <a href="/consultancies/kangaroo-education-foundation-pvt.-ltd."
+  <Table>
+    <TableHead>
+      <TableHeadCell>aaaaaa</TableHeadCell>
+      <TableHeadCell>bbbbb</TableHeadCell>
+    </TableHead>
+    <TableBody tableBodyClass="divide-y">
+      <TableBodyRow>
+        <TableBodyCell>Standard Test Fee</TableBodyCell>
+        <TableBodyCell>Covers the test and result issuance</TableBodyCell>
+      </TableBodyRow>
+      <TableBodyRow>
+        <TableBodyCell>Additional Costs</TableBodyCell>
+        <TableBodyCell
+          >Additional copies of results may cost around $20 each</TableBodyCell
+        >
+      </TableBodyRow>
+      <TableBodyRow>
+        <TableBodyCell>Cancellation and Rescheduling</TableBodyCell>
+        <TableBodyCell
+          >Partial refunds may be available for cancellations over five weeks
+          before the test, <br /> rescheduling fees also apply</TableBodyCell
+        >
+      </TableBodyRow>
+      <TableBodyRow>
+        <TableBodyCell>Preparation Costs</TableBodyCell>
+        <TableBodyCell>
+          Budget for preparation materials or courses to boost your score.</TableBodyCell
+        >
+      </TableBodyRow>
+    </TableBody>
+  </Table>
+
+  <br />
+  <Heading tag="h5">Top Consultancies for IELTS</Heading>
+  <List position="inside">
+    <Li
+      ><a href="/consultancies/kangaroo-education-foundation-pvt.-ltd."
         >Kangaroo Education Foundation</a
-      >
-    </li>
-    <li><a href="/consultancies/global-reach-nepal">Global Reach Nepal</a></li>
-    <li>
-      <a href="/consultancies/abroad-study-center">Abroad Study Center</a>
-    </li>
-    <li><a href="/consultancies/aecc-global">AECC Global</a></li>
-  </ul>
+      ></Li
+    >
+    <Li><a href="/consultancies/education-planet">Education Planet</a></Li>
+    <Li><a href="/consultancies/global-reach-nepal">Global Reach Nepal</a></Li>
+    <Li><a href="/consultancies/abroad-study-center">Abroad Study Center</a></Li
+    >
+    <Li><a href="/consultancies/aecc-global">AECC Global</a></Li>
+  </List>
 
-  <h2>Conclusion</h2>
+  <br />
+  <Heading tag="h5">Conclusion</Heading>
   <p>
     Achieving a high score on the IELTS opens doors to global opportunities in
     education, employment, and immigration. Whether you're aiming for university
