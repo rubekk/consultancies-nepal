3594369ce7a1bf9be81274720cb5b2090f623167
Rubek Maharjan <rubekmhzn7@gmail.com>
2025-01-30 00:18:44 +0545

    fix: consultancy page simple design

--- src/routes/consultancies/[slug]/+page.svelte
+++ src/routes/consultancies/[slug]/+page.svelte
@@ -1,180 +1,151 @@
 <script>
 	import "./../style.css";
 	import { countriesList, testList } from "$lib/index.js";
-	import { initializeApp } from "firebase/app";
-    import { getFirestore, collection, addDoc } from "firebase/firestore";
-    import { firebaseConfig } from "$lib/firebaseConfig.js";
-	import { Card, Badge, Button } from "flowbite-svelte";
-	import { ArrowRightOutline } from "flowbite-svelte-icons";
+	import { Badge } from "flowbite-svelte";
 
-    const app = initializeApp(firebaseConfig);
-    const db = getFirestore(app); 
-    const colRef= collection(db, "enquire");
+	export let data = {
+		name: "",
+		address: "",
+		phone: "",
+		about: "",
+		socials: {
+			facebook: "",
+			instagram: "",
+			youtube: "",
+			linkedin: "",
+		},
+		images: ["", ""],
+		testPreparations: [],
+		countriesFocused: [],
+	};
 
-	export let data= {
-        name: "AECC Global",
-        address: "Kamal Pokhari",
-        phone: "01-5970315",
-        about: "AECC Global is a leading global international education consultancy, headquartered in Melbourne, Australia. We have supported the study abroad dreams of 50K+ international students, connecting them to the right study opportunities through over 750+ top educational institutions. AECC Global's EdTEch is a unified digital ecosystem that makes international education research and application a seamless self-guided process and is supported by our expert education guidance counsellors. AECC Global's strength lies in its 450+ highly committed employees across 42 branches in 15 countries and growing. Almost all our Education Counsellors have been international students at some point in their lives, so we really understand what it’s like to be considering international study.",
-        socials: {
-            facebook: "https://www.facebook.com/AECCNepal/",
-            instagram: "https://www.instagram.com/aecc.nepal/",
-            youtube: "https://www.youtube.com/channel/UCCtwo7tqTfkr__pio4WQCkg",
-            linkedin: "https://www.linkedin.com/company/aecc-global-nepal/"
-        },
-        images: [
-            "https://lh3.googleusercontent.com/p/AF1QipODIg-OLM7l5La7DiN2unyh6ztQwCJdhv9rTkdw=s0", 
-            "https://lh3.googleusercontent.com/p/AF1QipMKv9o_JWGLzFAotMIIq_nN_Rb3zcKak9s7-ciV=s0"
-        ],
-        testPreparations: ["IELTS", "TOEFL", "PTE", "GRE", "GMAT"],
-        countriesFocused: ["Australia", "Canada", "UK", "USA", "New Zealand", "France", "Cyprus", "Germany", "Switzerland", "UAE"]
-    };
-	
-	let currImgUrl= data.images[0],
-		activeImg= 0,
-		openEnquiry= false,
-		formData= {
-			name: "",
-			email: "",
-			phone: "",
-			country: "Australia",
-			message: ""
-		};
+	const euroCountries = [
+		"france",
+		"estonia",
+		"italy",
+		"denmark",
+		"greece",
+		"portugal",
+		"poland",
+		"finland",
+		"australia",
+		"norway",
+		"switzerland",
+		"cyprus",
+		"ireland",
+		"mauritius",
+	];
 
-	const changeImage= i=>{
-		currImgUrl= data.images[i];
-		activeImg= i;
-	}
+	const checkCountry = (country) => {
+		let lowCountry = country.toLowerCase();
 
-	const checkCountry= country=>{
-		let euroCountries= ["france", "estonia", "italy", "denmark", "greece", "portugal", "poland", "finland", "australia", "norway", "switzerland", "cyprus", "ireland", "mauritius"];
-		let lowCountry= country.toLowerCase();
-
-		if(euroCountries.includes(lowCountry)) lowCountry= "europe";
-		else if(lowCountry=="uk") lowCountry= "united kingdom";
+		if (euroCountries.includes(lowCountry)) lowCountry = "europe";
+		else if (lowCountry == "uk") lowCountry = "united kingdom";
 
 		return countriesList.includes(lowCountry);
-	}
+	};
 
-	const getCountryURL= country=>{
-		let euroCountries= ["france", "estonia", "italy", "denmark", "greece", "portugal", "poland", "finland",  "norway", "switzerland", "cyprus", "ireland", "mauritius"];
-		let lowCountry= country.toLowerCase();
+	const getCountryURL = (country) => {
+		let lowCountry = country.toLowerCase();
 
-		if(euroCountries.includes(lowCountry)) lowCountry= "europe";
-		else if(lowCountry=="uk") lowCountry= "united kingdom";
-		
-		return `/study-abroad/${lowCountry.replaceAll(" ","-")}`;
-	}
+		if (euroCountries.includes(lowCountry)) lowCountry = "europe";
+		else if (lowCountry == "uk") lowCountry = "united kingdom";
 
-	const checkTest= test=>{
-		let lowTest= test.toLowerCase();
+		return `/study-abroad/${lowCountry.replaceAll(" ", "-")}`;
+	};
+
+	const checkTest = (test) => {
+		let lowTest = test.toLowerCase();
 
 		return testList.includes(lowTest);
-	}
+	};
 
-	const handleSubmit= async ()=>{
-        if(!formData.name || !formData.email || !formData.phone || !formData.country || !formData.message) return;
-
-        await addDoc(colRef, formData);
-        formData= {
-			name: "",
-			email: "",
-			phone: "",
-			country: "Australia",
-			message: ""
-		}
-
-		openEnquiry= false;
-    }
+	console.log(data);
 </script>
 
 <svelte:head>
-  <title>StudyAbroadNP - {data.name}</title>
+	<title>StudyAbroadNP - {data.name}</title>
 </svelte:head>
 
-{#if data.address && data.phone}
-<div class="consultancy-container">
-	<Card>
-		<h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{data.name}</h5>
-		<p class="mb-3 font-normal text-gray-700 dark:text-gray-400 leading-tight">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
-		<Button class="w-fit">
-			Read more <ArrowRightOutline class="w-6 h-6 ms-2 text-white" />
-		</Button>
-	</Card>
-	<div class="consultancy-data">
-		<div class="consultancy-data-bottom">
-			<Badge color="indigo">About</Badge>
-			<p>{data.about}</p>
-			<Badge color="indigo">Major countries</Badge>
+{#if data.name && data.address}
+	<div class="consultancy-container">
+		<div class="consultancy-data">
+			<h1 class="text-3xl">{ data.name }</h1>
+			<div class="consultancy-location m-2 text-gray-600">
+				<i class="fa-solid fa-location-dot"></i> <span class="ml-2 italic">{ data.address }</span>
+			</div>
+			<div class="socials">
+				<a
+					href={data.socials.facebook}
+					target="_blank"
+					aria-label="Facebook link"
+				>
+					<i class="fa-brands fa-square-facebook"></i>
+				</a>
+				<a
+					href={data.socials.instagram}
+					target="_blank"
+					aria-label="Instagram link"
+				>
+					<i class="fa-brands fa-instagram"></i>
+				</a>
+				<a
+					href={data.socials.youtube}
+					target="_blank"
+					aria-label="Youtube link"
+				>
+					<i class="fa-brands fa-youtube"></i>
+				</a>
+				<a
+					href={data.socials.linkedin}
+					target="_blank"
+					aria-label="LinkedIn link"
+				>
+					<i class="fa-brands fa-linkedin"></i>
+				</a>
+			</div>
+			<Badge class="mt-8 mb-4 text-[1.2rem]" color="indigo">About</Badge>
+			<p>
+				{data.about ||
+					"Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates cum animi enim consequuntur veritatis, dignissimos cupiditate quas minus nihil eligendi in dicta ad quo autem praesentium incidunt officiis ut voluptatibus accusamus. Eveniet sed ut expedita, voluptas at, adipisci aperiam velit nesciunt ducimus, laboriosam minus ipsam nam molestias. Repellendus quod non doloribus? Voluptas, aspernatur totam pariatur sapiente, laboriosam soluta adipisci aliquid dolorum modi odit ex voluptatem placeat optio quam iusto provident, nostrum dolore. Enim tempora possimus, recusandae et minus cumque ut fugit mollitia officia sed harum asperiores nobis id, expedita nihil dolore minima."}
+			</p>
+			<Badge class="mt-8 mb-4 text-[1.2rem]" color="indigo">Major countries</Badge>
 			<div class="data-countries">
 				{#each data.countriesFocused as country}
 					{#if checkCountry(country)}
-						<Badge large color="dark">
+						<Badge class="mx-2" large color="dark">
 							<a href={getCountryURL(country)}>{country}</a>
 						</Badge>
 					{:else}
-						<Badge large color="dark">{country}</Badge>
+						<Badge class="mx-2" large color="dark">{country}</Badge>
 					{/if}
 				{/each}
 			</div>
-			<Badge color="indigo">Test preparations</Badge>
+			<Badge class="mt-8 mb-4 text-[1.2rem]" color="indigo">Test preparations</Badge>
 			<div class="data-tests">
 				{#each data.testPreparations as test}
 					{#if checkTest(test)}
-						<Badge large color="dark">
-							<a href={`/test-guides/${test.toLowerCase()}`}>{test}</a>
+						<Badge class="mx-2" large color="dark">
+							<a href={`/test-guides/${test.toLowerCase()}`}
+								>{test}</a
+							>
 						</Badge>
 					{:else}
 						<Badge large color="dark">{test}</Badge>
 					{/if}
 				{/each}
 			</div>
-			<Badge color="indigo">Test preparations</Badge>
-			<iframe src={data.mapURL} width="100%" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
-			<div class="socials">
-				<span>Social links:</span>
-				<a href={data.socials.facebook} target="_blank">
-					<i class="fa-brands fa-square-facebook"></i>
-				</a>
-				<a href={data.socials.instagram} target="_blank">
-					<i class="fa-brands fa-instagram"></i>
-				</a>
-				<a href={data.socials.youtube} target="_blank">
-					<i class="fa-brands fa-youtube"></i>
-				</a>
-				<a href={data.socials.linkedin} target="_blank">
-					<i class="fa-brands fa-linkedin"></i>
-				</a>
-			</div>
+			<Badge class="mt-8 mb-4 text-[1.2rem]" color="indigo">Map</Badge>
+			<iframe
+				src={data.mapURL}
+				width="100%"
+				height="450"
+				style="border:0;"
+				allowfullscreen=""
+				loading="lazy"
+				referrerpolicy="no-referrer-when-downgrade"
+				title="Consultancy map"
+			></iframe>
 		</div>
 	</div>
-</div>
-
-{#if openEnquiry}
-<div class="enquire-modal">
-	<div class="enquire-modal-inner">
-		<i class="cross-modal fa-solid fa-times" on:click={()=> openEnquiry=false}></i>
-		<h2>Have any queries? We will reach out to you soon!</h2>
-		<form>
-			<input bind:value={formData.name} type="text" placeholder="Full name" required>
-			<input bind:value={formData.email} type="email" placeholder="Email" required>
-			<input bind:value={formData.phone} type="number" placeholder="Phone number" required>
-			<select bind:value={formData.country} placeholder="Select a country" required>
-				<option value="Australia">Australia</option>
-				<option value="USA">USA</option>
-				<option value="Canada">Canada</option>
-				<option value="Germany">Germany</option>
-				<option value="New Zealand">New Zealand</option>
-				<option value="United Kingdom">United Kingdom</option>
-				<option value="Japan">Japan</option>
-				<option value="Singapore">Singapore</option>
-				<option value="South Korea">South Korea</option>
-				<option value="Help me decide">Others / Help me decide</option>
-			</select>
-			<textarea bind:value={formData.message} placeholder="Enter your queries..." required></textarea>
-			<button class="submit" on:click={handleSubmit}>Submit</button>
-		</form>
-	</div>
-</div>
-{/if}
 {/if}
